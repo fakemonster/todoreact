bbdeeff6b3bd8c7e46c179e665b8fabbbfe27053
Sam Goldberg <sgoldber61@gmail.com>
2018-08-04 16:18:19 -0700

    minimal start to project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<div>Hello!</div>, document.getElementById('root'));
 
